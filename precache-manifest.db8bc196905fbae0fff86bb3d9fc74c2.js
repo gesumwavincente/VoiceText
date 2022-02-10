bfec10592ce6cc085d3dc86c23d9c3839a72016e
Vincent Nyagaka <gesumwavincente@gmail.com>
2022-02-10 15:59:21 +0300

    Updates

---
+++ precache-manifest.db8bc196905fbae0fff86bb3d9fc74c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f16371231a34847efbbcc41e49dd02d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5428437b7fc81e63f64d",
+    "url": "/static/css/main.2f6036a2.chunk.css"
+  },
+  {
+    "revision": "ec9f1f683abd4c53a16f",
+    "url": "/static/js/2.e962d47a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e962d47a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5428437b7fc81e63f64d",
+    "url": "/static/js/main.e979de84.chunk.js"
+  },
+  {
+    "revision": "ea6a6b9a2e76037267b3",
+    "url": "/static/js/runtime-main.869c5450.js"
+  }
+]);
